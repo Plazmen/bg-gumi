d5833ad91a6b7a62a69779be32866727e19d9705
Plazmen <plazmen1@abv.bg>
2015-07-23 13:58:58 +0300

    installation

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/bg-gumi/admin/');
+define('HTTP_CATALOG', 'http://localhost/bg-gumi/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/bg-gumi/admin/');
+define('HTTPS_CATALOG', 'http://localhost/bg-gumi/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/wamp/www/bg-gumi/admin/');
+define('DIR_SYSTEM', 'C:/wamp/www/bg-gumi/system/');
+define('DIR_LANGUAGE', 'C:/wamp/www/bg-gumi/admin/language/');
+define('DIR_TEMPLATE', 'C:/wamp/www/bg-gumi/admin/view/template/');
+define('DIR_CONFIG', 'C:/wamp/www/bg-gumi/system/config/');
+define('DIR_IMAGE', 'C:/wamp/www/bg-gumi/image/');
+define('DIR_CACHE', 'C:/wamp/www/bg-gumi/system/cache/');
+define('DIR_DOWNLOAD', 'C:/wamp/www/bg-gumi/system/download/');
+define('DIR_UPLOAD', 'C:/wamp/www/bg-gumi/system/upload/');
+define('DIR_LOGS', 'C:/wamp/www/bg-gumi/system/logs/');
+define('DIR_MODIFICATION', 'C:/wamp/www/bg-gumi/system/modification/');
+define('DIR_CATALOG', 'C:/wamp/www/bg-gumi/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'bg-gumi');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/bg-gumi/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/bg-gumi/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/wamp/www/bg-gumi/catalog/');
+define('DIR_SYSTEM', 'C:/wamp/www/bg-gumi/system/');
+define('DIR_LANGUAGE', 'C:/wamp/www/bg-gumi/catalog/language/');
+define('DIR_TEMPLATE', 'C:/wamp/www/bg-gumi/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/wamp/www/bg-gumi/system/config/');
+define('DIR_IMAGE', 'C:/wamp/www/bg-gumi/image/');
+define('DIR_CACHE', 'C:/wamp/www/bg-gumi/system/cache/');
+define('DIR_DOWNLOAD', 'C:/wamp/www/bg-gumi/system/download/');
+define('DIR_UPLOAD', 'C:/wamp/www/bg-gumi/system/upload/');
+define('DIR_MODIFICATION', 'C:/wamp/www/bg-gumi/system/modification/');
+define('DIR_LOGS', 'C:/wamp/www/bg-gumi/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'bg-gumi');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
